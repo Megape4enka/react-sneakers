bbcbeab86f0d16968bb4db0cd9ca146cdb28fbec
HP <mega.pe4enka@gmail.com>
2021-08-12 18:16:01 +0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,47 +1,30 @@
 import React from 'react';
 import './index.scss'
+import Card from './components/Card'
+import Header from './components/Header'
+import Drawer from './components/Drawer'
 
 function App() {
   return (
     <div className="wrapper clear">
-      <header className='d-flex justify-between align-center p-40'>
-        <div className='d-flex aling-center'>
-          <img width={40} height={40} src="/img/logo.png" />
-          <div>
-            <h3 className='text-uppercase'>React Sneakers</h3>
-            <p className='opacity-5'>Магазин лучших кросовок</p>
-          </div>
-        </div>
-        <ul className='d-flex'>
-          <li className='mr-30'>
-            <img width={18} height={18} src="/img/cart.svg" />
-            <span>1205 руб.</span>
-          </li>
-          <li>
-            <img width={18} height={18} src="/img/user.svg" />
-          </li>
-        </ul>
-      </header>
+      <Drawer />
+      <Header />
 
       <div className="content p-40">
-        <h1 className='mb-40'>Все кросовки</h1>
+        <div className='mb-40 d-flex align-center justify-between'>
+          <h1>Все кросовки</h1>
+          <div className='search-block d-flex'>
+            <img src="/img/search.svg" alt="Search"/>
+            <input type="text" placeholder='Поиск...'/>
+          </div>
+
+        </div>
 
         <div className="d-flex">
-          <div className='card'>
-              <img src="/img/sneakers/1.jpg" width={133} height={112} />
-              <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-              <div className='d-flex justify-between align-center'>
-                <div className='d-flex flex-column'>
-                  <span>Цена:</span>
-                  <b>12 999 руб.</b>
-                </div>
-                <button className='button'>
-                  <img src="/img/plus.svg" width={11} height={11}/>
-                </button>
-              </div>
-            </div>
-          </div>
+          <Card />
         </div>
+
+      </div>
 
     </div>
   );
